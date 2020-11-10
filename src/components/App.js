9d009c0496abf4aa7cd41be9d2fa40f5e300901d
aravindbhaskar41 <aravindbhaskar41@gmail.com>
2020-11-11 02:43:15 +0530

    Requirements done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-ns-test="project-name">Timesheet</h1>
+      <p data-ns-test="project-description">
+        The project is to develop a timesheet app
+      </p>
+    </>
+  );
+};
 
 export default App;
